abe6ebf607432944bbcec2a552558e621f370233
Bhargodev Arya <barya@sapient.com>
2016-10-13 15:11:01 +0530

    multiThreading print 1,2,3

--- src/main/java/com/bhargo/Main.java
+++ src/main/java/com/bhargo/Main.java
@@ -12,14 +12,104 @@ public class Main {
         //multipleProdCons();
         ExecutorService executorService = Executors.newScheduledThreadPool(2);
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-        System.out.println(executor.getMaximumPoolSize() + " " + executor.getCorePoolSize());
+        //System.out.println(executor.getMaximumPoolSize() + " " + executor.getCorePoolSize());
         //executor.submit(() -> {});
 
         //findHighestTwoNumbers(new Integer[]{54,856,86,12,4,66,856,35});
 
+        multiThreadingPrintNumbers();
+
     }
 
-    public static void findHighestTwoNumbers (Integer[] arr) {
+    @FunctionalInterface
+    private interface func<T> {
+        T produce ();
+    }
+
+    private static void multiThreadingPrintNumbers() {
+
+        BlockingQueue<Integer> sendingQueue1 = new ArrayBlockingQueue<>(1);
+        BlockingQueue<Integer> sendingQueue2 = new ArrayBlockingQueue<>(1);
+        BlockingQueue<Integer> sendingQueue3 = new ArrayBlockingQueue<>(1);
+
+
+        int[] intArr1 = new int[]{1};
+        int[] intArr2 = new int[]{2};
+        int[] intArr3 = new int[]{3};
+        new Thread(new userRunnable<Integer>(sendingQueue3, sendingQueue1, true, () -> {
+            if (intArr1[0] == 1) {
+                intArr1[0] = 4;
+                return new Integer(1);
+            } else if (intArr1[0] == 4) {
+                intArr1[0] = 7;
+                return new Integer(4);
+            } else {
+                intArr1[0] = 1;
+                return new Integer(7);
+            }
+        })).start();
+        new Thread(new userRunnable<Integer>(sendingQueue1, sendingQueue2, false, () -> {
+            if (intArr2[0] == 2) {
+                intArr2[0] = 5;
+                return new Integer(2);
+            } else if (intArr2[0] == 5) {
+                intArr2[0] = 8;
+                return new Integer(5);
+            } else {
+                intArr2[0] = 2;
+                return new Integer(8);
+            }
+        })).start();
+        new Thread(new userRunnable<Integer>(sendingQueue2, sendingQueue3, false, () -> {
+            if (intArr3[0] == 3) {
+                intArr3[0] = 6;
+                return new Integer(3);
+            } else if (intArr3[0] == 6) {
+                intArr3[0] = 9;
+                return new Integer(6);
+            } else {
+                intArr3[0] = 3;
+                return new Integer(9);
+            }
+
+        })).start();
+
+    }
+    private static class userRunnable<T> implements Runnable {
+
+        private BlockingQueue<T> receivingQueue;
+        private BlockingQueue<T> sendingQueue;
+        private boolean init;
+        private func<T> func;
+
+        public userRunnable(BlockingQueue<T> receivingQueue, BlockingQueue<T> sendingQueue, boolean init, func func) {
+            this.receivingQueue = receivingQueue;
+            this.sendingQueue = sendingQueue;
+            this.init = init;
+            this.func = func;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(2000);
+                    if(init) {
+                        sendingQueue.put(func.produce());
+                        init = false;
+                    }
+                    if(receivingQueue.size() > 0) {
+                        System.out.println(receivingQueue.take());
+                        sendingQueue.put(func.produce());
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void findHighestTwoNumbers (Integer[] arr) {
         int highest, secondHighest;
         if(arr[0] > arr[1]) {
             highest = arr[0];
@@ -41,11 +131,11 @@ public class Main {
         System.out.printf("The largest num is %d and the 2nd largest is %d", highest, secondHighest);
     }
 
-    public static void sortMapByValues (Map<Integer, String> map) {
+    private static void sortMapByValues (Map<Integer, String> map) {
         Set<String> set = new TreeSet<>(map.values());
     }
 
-    public static void multipleProdCons () {
+    private static void multipleProdCons () {
         BlockingDeque<Resource> blockingDeque = new LinkedBlockingDeque<>();
         for(int i =1; i<=10;i++) {
             new Thread(new Producer(blockingDeque)).start();
@@ -56,7 +146,7 @@ public class Main {
         }
     }
 
-    static class Resource {
+    private static class Resource {
         private String str;
 
         public Resource(String str) {
@@ -77,7 +167,7 @@ public class Main {
         }
     }
 
-    static class Producer implements Runnable {
+    private static class Producer implements Runnable {
 
         private BlockingDeque<Resource> blockingDeque;
 
@@ -101,7 +191,7 @@ public class Main {
         }
     }
 
-    static class Consumer implements Runnable {
+    private static class Consumer implements Runnable {
 
         private BlockingDeque<Resource> blockingDeque;
 
